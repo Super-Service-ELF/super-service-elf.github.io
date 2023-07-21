b3b42161b1ce59984938989876c7f421da395143
E2Z1 <ezracentner@gmx.de>
2023-07-21 16:23:47 +0200

    mobile compatible & instead iframe js

--- index.js
+++ index.js
@@ -1,49 +1,12 @@
-var smallmenu = false
-
-var links = document.getElementsByClassName("link");
-var navbar = document.getElementById("navbar");
-
-function createLink(text, link, id, clazz) {
-    var liobj = document.createElement("li");
-    var aobj = document.createElement("a");
-    aobj.href = link
-    aobj.appendChild(document.createTextNode(text));
-    liobj.appendChild(aobj);
-    liobj.id = id;
-    liobj.className = clazz;
-    return liobj
+window.onload = function () {
+    var navbar = document.getElementById("navbar")
 }
-
-
-function resize() {
-    if (window.innerWidth < 1406 && !smallmenu) {
-        for (let i = 0, len = links.length; i < len; i++) {
-            links[i].remove()
-          } 
-        navbar.appendChild(createLink("Menu","","menubtnsmall",""))
-        smallmenu = true
+function togglemenu() {
+    if (navbar.style.display == "") {
+        navbar.style.display = "block";
+        document.getElementById("header").style.height="800px";
+    }   else {
+        navbar.style.display = "";
+        document.getElementById("header").style.height="180px";
     }
-
-    if (window.innerWidth >= 1406 && smallmenu) {
-        document.getElementById("menubtnsmall").remove()
-        navbar.appendChild(createLink("Home","/","","link"))
-        navbar.appendChild(createLink("Über","/ueber/","","link"))
-        navbar.appendChild(createLink("Newsletter","/newsletter/","","link"))
-        navbar.appendChild(createLink("Newsletter-Archiv","/newsletter-archiv/","","link"))
-        navbar.appendChild(createLink("Geschichte","/geschichte/","","link"))
-        navbar.appendChild(createLink("Bewerten","/bewerten/","","link"))
-        var aobj = document.createElement("a");
-        aobj.href = "/auftrag/"
-        aobj.appendChild(document.createTextNode("Auftrag aufgeben"));
-        aobj.id = "auftrag-button";
-        aobj.className = "link";
-        document.getElementById("headerheader").appendChild(aobj)
-
-        smallmenu = false
-
-    }
-
 }
-
-window.onresize = resize;
-window.onload = resize;
