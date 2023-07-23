d37134e148449f98bf6c57cdfe9014c4a1d8d887
Super-Service-ELF <mail@super-service-elf.de>
2023-07-23 17:43:41 +0200

    few changes
    added subsites, added example text, added rotation onclick mobile-menu-button,

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function togglemenu() {
     if (navbar.style.display == "") {
         navbar.style.display = "block";
         document.getElementById("header").style.height="800px";
+        document.getElementById("mobile-menu-button").style.animation="rotate90 0.3s ease-out forwards";
     }   else {
         navbar.style.display = "";
         document.getElementById("header").style.height="180px";
+        document.getElementById("mobile-menu-button").style.animation="rotate0 0.3s ease-out";
     }
 }
