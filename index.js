e6bfb6fe4dc8355365219e17aa443ba3efb179fc
E2Z1 <ezracentner@gmx.de>
2023-07-25 17:04:25 +0200

    bug fixes
    when changing from mobile to desktop website while navbar being expanded, header staying big: fixed
    
    the user is now unable to select "Auftrag aufgeben"
    
    made spaces consistent

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-window.onload = function () {
-    var navbar = document.getElementById("navbar")
-}
 function togglemenu() {
     if (navbar.style.display == "") {
         navbar.style.display = "block";
@@ -10,5 +7,14 @@ function togglemenu() {
         navbar.style.display = "";
         document.getElementById("header").style.height="180px";
         document.getElementById("mobile-menu-button").style.animation="rotate0 0.3s ease-out";
+    }
+}
+
+window.onresize = function () {
+    if (window.innerWidth > 1400) {
+        navbar.style.display = "";
+        document.getElementById("header").style.height="180px";
+        document.getElementById("mobile-menu-button").style.animation="rotate0 0s ease-out";
+
     }
 }
