13d9825d2e14dbf97e182e5277d060860deb1502
Super-Service-ELF <mail@super-service-elf.de>
2023-08-01 19:59:02 +0200

    Merge load_header&footer.js into index.js.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function loadExternalHTML(targetID) {
+    var url = "/content/" + targetID + ".html";
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            var target = document.getElementById(targetID);
+            target.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+loadExternalHTML("header");
+loadExternalHTML("footer");
 function togglemenu() {
     if (navbar.style.display == "") {
         navbar.style.display = "block";
